a1fade6b4f19fef9c89585add37f1db5e30e60c8
Knight-JXNU <1990785022@qq.com>
2016-11-17 19:25:48 +0800

    常量

---
+++ src/main/java/com/common/Constant.java
@@ -0,0 +1,12 @@
+package com.common;
+
+/**
+ * Created by Knight_JXNU on 2016/11/17.
+ */
+public class Constant {
+
+    public static String SUCCESS_MES = "SUCCESS";
+    public static String FAILED_MES = "FAILED";
+    public static String TEST_MES = "TEST";
+
+}
