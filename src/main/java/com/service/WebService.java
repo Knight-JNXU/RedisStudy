64b7ca6349538f7ed44d69d1a7f2a7bb7f871d45
Knight-JXNU <1990785022@qq.com>
2016-11-17 19:26:19 +0800

    service

--- src/main/java/com/service/WebService.java
+++ src/main/java/com/service/WebService.java
@@ -1,8 +1,32 @@
 package com.service;
 
+import com.common.Constant;
+import com.dao.WebDao;
+import com.model.WebModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Knigh on 2016/11/16.
  */
+@Service
 public class WebService extends BaseService {
 
+    @Autowired
+    private WebDao webDao;
+
+    public String saveWeb(WebModel web) throws Exception{
+//        return Constant.TEST_MES;
+        try {
+            webDao.saveWeb(web);
+            return Constant.SUCCESS_MES;
+        }catch (Exception e){
+            e.printStackTrace();
+            return Constant.FAILED_MES;
+        }
+    }
+
+    public String getWeb() throws Exception{
+        return webDao.getWeb().toString();
+    }
 }
