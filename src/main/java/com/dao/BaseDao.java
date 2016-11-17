acc1fd1224dd1dd6bce7469edac5122f5aa3a2d5
Knight-JXNU <1990785022@qq.com>
2016-11-17 19:57:24 +0800

    update

--- src/main/java/com/dao/BaseDao.java
+++ src/main/java/com/dao/BaseDao.java
@@ -1,7 +1,38 @@
 package com.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
 /**
  * Created by Knigh on 2016/11/14.
  */
 public class BaseDao {
+    @Autowired
+    protected RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    protected Object get(String key){
+        return redisTemplate.boundValueOps(key).get();
+    }
+
+    /**
+     *
+     * @param key
+     * @param value
+     */
+    protected void set(String key, Object value){
+        redisTemplate.boundValueOps(key).set(value);
+    }
+
+    /**
+     *
+     * @param key
+     */
+    protected void del(String key){
+        redisTemplate.delete(key);
+    }
 }
--- src/main/java/com/dao/WebDao.java
+++ src/main/java/com/dao/WebDao.java
@@ -1,5 +1,6 @@
 package com.dao;
 
+import com.model.WebModel;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -7,4 +8,18 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class WebDao extends BaseDao{
+    public boolean saveWeb(final WebModel web) throws Exception{
+        try {
+            set(web.getDes(), web.getUrl());
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public WebModel getWeb() throws Exception{
+        Object o = get("baidu");
+        return (WebModel)o;
+    }
 }
