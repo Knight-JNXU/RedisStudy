c61ca9480af7a5860d80a983918157b4008eef97
Knight-JXNU <1990785022@qq.com>
2016-11-17 19:27:15 +0800

    controller

--- src/main/java/com/controller/WebController.java
+++ src/main/java/com/controller/WebController.java
@@ -1,7 +1,10 @@
 package com.controller;
 
 import com.model.WebModel;
+import com.service.WebService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,11 +16,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/web")
 public class WebController extends BaseController {
 
+    @Autowired
+    private WebService webService;
+
     @ResponseBody
     @RequestMapping(value = "/input", method = RequestMethod.POST)
-    public void input(WebModel web){
-        System.out.println(web.getUrl());
-        System.out.println(web.getDes());
+    public void input(WebModel web) throws Exception{
+        System.out.println("input:");
+        System.out.println(web);
+        webService.saveWeb(web);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/getweb", method = RequestMethod.GET)
+    public void getWeb() throws Exception{
+        System.out.println("output:");
+        System.out.println(webService.getWeb());
     }
 
 }
